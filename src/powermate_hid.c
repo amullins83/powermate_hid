d4090b17b9feec16e2d54106532df3e9c186609b
Austin Mullins <austin.mullins@gmail.com>
2014-06-26 01:28:03 -0500

    Completely re-implemented with libusb.

--- src/powermate_hid.c
+++ src/powermate_hid.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "hidapi.h"
 #include "libusb.h"
-
-#define POWERMATE_CONTROL_REQUEST_LENGTH 5
-#define POWERMATE_INPUT_LENGTH 8
+#include <time.h>
 
 struct powermate_hid_internals {
-	hid_device    *handle;
-	unsigned char *buffer;
+    libusb_context         *context;
+    libusb_device         **dev_list;
+    libusb_device          *device;   
+    libusb_device_handle   *handle;
+    struct libusb_transfer *input_transfer;
+    struct libusb_transfer *control_transfer;
+    struct timeval         *tv;
+    int                    input_lock;
+    int                    control_lock; 
+    unsigned char *buffer;
 };
 
 PowermateInternals *internals_new(void);
@@ -18,132 +23,147 @@ void internals_delete(PowermateInternals *this);
 
 PowermateHid *powermate_hid_new(void)
 {
-	PowermateHid *powermate = malloc(sizeof *powermate);
+    PowermateHid *powermate = malloc(sizeof *powermate);
 
-	if(powermate != NULL)
-	{
-		powermate->_internals = internals_new();
-		if(powermate->_internals != NULL)
-		{
-			return powermate;
-		}
-	}
+    if(powermate == NULL)
+    {
+        printf("Failed to allocate PowermateHid\n");
+        return NULL;
+    }
 
-	return NULL;
+    powermate->_internals = internals_new();
+    
+
+    if(powermate->_internals == NULL)
+    {
+        printf("Failed to create internals\n");
+        free(powermate);
+        return NULL;
+    }
+
+    return powermate;
 }
 
 void powermate_hid_delete(PowermateHid *this)
 {
-	internals_delete(this->_internals);
-	free(this);
+    internals_delete(this->_internals);
+    free(this);
 }
+
+void on_input_done(struct libusb_transfer *transfer);
 
 // These return 0 if all is well, otherwise a powermate_hid_error code
 PowermateError powermate_hid_get_input(PowermateHid *this)
 {
-	int read_bytes = hid_get_feature_report(
-		this->_internals->handle,
-		this->_internals->buffer,
-		8);
+    int error = POWERMATE_HID_SUCCESS;
+
+    PowermateInternals *pmints = this->_internals;
+
+    libusb_handle_events_timeout_completed(pmints->context,
+        pmints->tv,
+        &pmints->input_lock);
+
+    pmints->input_lock = 0;
     
-    if(read_bytes < 0)
+    libusb_fill_interrupt_transfer(
+      pmints->input_transfer,
+      pmints->handle,
+      POWERMATE_IN_ENDPOINT,
+      (unsigned char*)pmints->buffer,
+      POWERMATE_IN_SIZE,
+      on_input_done,
+      this,
+      1000);
+
+    error = libusb_submit_transfer(pmints->input_transfer);
+    
+    if(error != LIBUSB_SUCCESS)
     {
-    	return POWERMATE_HID_ERROR_ACCESS;
-    }
-    else if(read_bytes < 6)
-    {
-    	return POWERMATE_HID_ERROR_TIMEOUT;
+      this->last_error = POWERMATE_HID_ERROR_UNKNOWN;
+      pmints->input_lock = 1;
     }
     else
     {
-    	this->last_input = (PowermateData){
-    		.button_state = this->_internals->buffer[0],
-    		.knob_displacement = this->_internals->buffer[1],
-    		._unused = 0,
-    		.led_brightness = this->_internals->buffer[3],
-    		.led_status = this->_internals->buffer[4],
-    		.led_multiplier = this->_internals->buffer[5]
-    	};
-
-    	return POWERMATE_HID_SUCCESS;
+      this->is_busy++;
+      this->last_error = POWERMATE_HID_SUCCESS;
     }
+    
+    return this->last_error;
+}
+
+void on_input_done(struct libusb_transfer *transfer)
+{
+    int error = transfer->status;
+    PowermateHid *this = (PowermateHid *)transfer->user_data;
+    PowermateInternals *pmints = this->_internals;
+
+    if(error == LIBUSB_SUCCESS)
+    {
+        this->last_input = (PowermateData){
+            .button_state = pmints->buffer[0],
+            .knob_displacement = pmints->buffer[1],
+            ._unused = 0,
+            .led_brightness = pmints->buffer[3],
+            .led_status = pmints->buffer[4],
+            .led_multiplier = pmints->buffer[5]
+        };
+
+        this->last_error = POWERMATE_HID_SUCCESS;
+    }
+    else
+    {
+        this->last_error = POWERMATE_HID_ERROR_TIMEOUT;
+    }
+
+    pmints->input_lock = 1;
+    this->is_busy--;
 }
 
 void on_output_done(struct libusb_transfer *transfer);
-
-libusb_device **dev_list = NULL;
-libusb_context *dev_context = NULL;
 
 int is_knob(libusb_device *dev);
 
 PowermateError powermate_hid_send_output(PowermateHid *this)
 {
-  libusb_device  *temp_dev, *knob_device = NULL;
-  libusb_device_handle *knob_handle = NULL;
-  libusb_init(&dev_context);
+    PowermateInternals *pmints = this->_internals;
+    int error = POWERMATE_HID_SUCCESS;
 
-  int dev_count = libusb_get_device_list(dev_context, &dev_list);
-  int i, error;
+    libusb_handle_events_timeout_completed(pmints->context,
+        pmints->tv, &pmints->control_lock);
 
-  for(i = 0; i < dev_count; i++)
-  {
-    temp_dev = dev_list[i];
-    if(is_knob(temp_dev))
+    pmints->control_lock = 0;
+
+    libusb_fill_control_setup(
+      pmints->buffer,
+      LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_INTERFACE |
+      LIBUSB_ENDPOINT_OUT,
+      1,
+      this->control.upper_value << 8 | this->control.type,
+      this->control.index,
+      0);
+
+    libusb_fill_control_transfer(
+      pmints->control_transfer,
+      pmints->handle,
+      pmints->buffer,
+      on_output_done,
+      this,
+      1000);
+
+    error = libusb_submit_transfer(pmints->control_transfer);
+
+    if(error)
     {
-        knob_device = temp_dev;
-        break;
+      this->last_error = POWERMATE_HID_ERROR_UNKNOWN;
+      pmints->control_lock = 1;
     }
-  }
+    else
+    {
+      this->is_busy++;
+      this->last_error = POWERMATE_HID_SUCCESS;
+    }
 
-  if(knob_device == NULL)
-  {
-  	libusb_free_device_list(dev_list, 1);
-  	return POWERMATE_HID_ERROR_NOT_FOUND;
-  }
-
-  error = libusb_open(knob_device, &knob_handle);
-
-  if(error || knob_handle == NULL)
-  {
-  	libusb_free_device_list(dev_list, 1);
-  	return POWERMATE_HID_ERROR_ACCESS;
-  }
-
-  struct libusb_transfer *output_transfer = libusb_alloc_transfer(0);
-
-  libusb_fill_control_setup(
-    this->_internals->buffer,
-    LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_INTERFACE |
-    LIBUSB_ENDPOINT_OUT,
-    1,
-    this->control.upper_value << 8 | this->control.type,
-    this->control.index,
-    0);
-
-  libusb_fill_control_transfer(
-    output_transfer,
-    knob_handle,
-    this->_internals->buffer,
-    on_output_done,
-    NULL,
-    1000);
-
-  error = libusb_submit_transfer(output_transfer);
-
-  if(error)
-  {
-  	libusb_close(knob_handle);
-  	libusb_free_device_list(dev_list, 1);
-  	libusb_free_transfer(output_transfer);
-  	libusb_exit(dev_context);
-  	dev_list = NULL;
-  	dev_context = NULL;
-  	return POWERMATE_HID_ERROR_UNKNOWN;
-  }
-
-  this->is_busy = 1;
-
-  return POWERMATE_HID_SUCCESS;
+    return this->last_error;
 }
 
 int is_knob(libusb_device *dev)
@@ -158,112 +178,210 @@ int is_knob(libusb_device *dev)
 
 void on_output_done(struct libusb_transfer *transfer)
 {
-	if(transfer->status != LIBUSB_SUCCESS)
-	{
-		printf("Control transfer failed: %d\n", transfer->status);
-	}
+    PowermateHid *this = (PowermateHid *)transfer->user_data;
 
-	PowermateHid *this = (PowermateHid *)transfer->user_data;
-    libusb_close(transfer->dev_handle);
-    libusb_free_device_list(dev_list, 1);
-	libusb_free_transfer(transfer);
-	dev_list = NULL;
-	libusb_exit(dev_context);
-	dev_context = NULL;
-	this->is_busy = 0;
+    if(transfer->status != LIBUSB_SUCCESS)
+    {
+        this->last_error = POWERMATE_HID_ERROR_TIMEOUT;
+    }
+    else
+    {
+        this->last_error = POWERMATE_HID_SUCCESS;
+    }
+
+    this->_internals->control_lock = 1;
+    this->is_busy--;
 }
 
 PowermateError powermate_hid_set_control(PowermateHid *this, PowermateControl control)
 {
-	this->control = control;
+    this->control = control;
 
-	// I could do some validation here
-	return POWERMATE_HID_SUCCESS;
+    // I could do some validation here
+    this->last_error = POWERMATE_HID_SUCCESS;
+    return this->last_error;
+}
+
+void null_internals(PowermateInternals *this);
+
+struct timeval tv_reset = {2, 0};
+
+
+void reset_tv(struct timeval *tv)
+{
+    memcpy(tv, &tv_reset, sizeof tv_reset);
 }
 
 PowermateInternals *internals_new(void)
 {
-	PowermateInternals *this = malloc(sizeof *this);
+    PowermateInternals *this = malloc(sizeof *this);
+    int device_count = 0, i, error;
+    libusb_device *temp = NULL;
 
-	if(this != NULL)
-	{
-		this->handle = hid_open(POWERMATE_VID, POWERMATE_PID, 0);
-		if(this->handle != NULL)
-		{
-			this->buffer = malloc(POWERMATE_INPUT_LENGTH);
-			
-			if(this->buffer == NULL)
-			{
-				return NULL;
-			}
-		}
-		else
-		{
-			return NULL;
-		}
-	}
+    if(this == NULL)
+    {
+        printf("\tFailed to allocate internals\n");
+        return NULL;
+    }
 
-	return this;
+    null_internals(this);
+
+    error = libusb_init(&this->context);
+
+    if(error)
+    {
+        printf("\tFailed to initialize context\n");
+        internals_delete(this);
+        return NULL;
+    }
+
+    device_count = libusb_get_device_list(this->context, &this->dev_list);
+
+    this->device = NULL;
+    
+    for(i = 0; i < device_count; i++)
+    {
+        temp = this->dev_list[i];
+        if(is_knob(temp))
+        {
+            this->device = temp;
+            break;
+        }
+    }
+
+    if(this->device == NULL)
+    {
+        printf("\tFailed to enumerate device\n");
+        internals_delete(this);
+        return NULL;
+    }
+
+    error += libusb_open(this->device, &this->handle);
+
+    if(error)
+    {
+        printf("\tFailed to get handle\n");
+        return NULL;
+    }
+
+    error += libusb_claim_interface(this->handle, 0);
+
+    if(error)
+    {
+        printf("\tFailed to claim interface\n");
+        return NULL;
+    }
+
+    this->buffer = malloc(POWERMATE_IN_SIZE);
+
+    if(this->buffer == NULL)
+    {
+        printf("\tFailed to allocate buffer\n");
+        return NULL;
+    }
+
+    this->input_transfer = libusb_alloc_transfer(0);
+    this->control_transfer = libusb_alloc_transfer(0);
+
+    if(error || this->input_transfer == NULL ||
+        this->control_transfer == NULL)
+    {
+        printf("\tFailed to allocate transfers\n");
+        internals_delete(this);
+        return NULL;
+    }
+
+    this->tv = malloc(sizeof *this->tv);
+
+    reset_tv(this->tv);
+
+    return this;
+}
+
+void null_internals(PowermateInternals *this)
+{
+    this->buffer = NULL;
+    this->handle = NULL;
+    this->dev_list = NULL;
+    this->input_transfer = NULL;
+    this->control_transfer = NULL;
+    this->context = NULL;
+    this->tv = NULL;
+    this->control_lock = 1;
+    this->input_lock = 1;
 }
 
 void internals_delete(PowermateInternals *this)
 {
-	free(this->buffer);
-	hid_close(this->handle);
-	free(this);
+    reset_tv(this->tv);
+    libusb_handle_events_timeout_completed(this->context,
+        this->tv, &this->input_lock);
+    libusb_handle_events_timeout_completed(this->context,
+        this->tv, &this->control_lock);
+
+    free(this->buffer);
+    libusb_close(this->handle);
+    libusb_free_device_list(this->dev_list, 1);
+    libusb_free_transfer(this->control_transfer);
+    libusb_free_transfer(this->input_transfer);
+    libusb_exit(this->context);
+    free(this->tv);
+    null_internals(this);
+
+    free(this);
 }
 
 
 PowermateControl powermate_control_fast_pulse = {
-	.type = POWERMATE_CONTROL_PULSE_MODE,
-	.upper_value = POWERMATE_PULSE_TABLE_NORMAL,
-	.index = 0x30 << 8 | POWERMATE_PULSE_SPEED_FAST
+    .type = POWERMATE_CONTROL_PULSE_MODE,
+    .upper_value = POWERMATE_PULSE_TABLE_NORMAL,
+    .index = 0x30 << 8 | POWERMATE_PULSE_SPEED_FAST
 };
 
 PowermateControl powermate_control_slow_pulse = {
     .type = POWERMATE_CONTROL_PULSE_MODE,
-	.upper_value = POWERMATE_PULSE_TABLE_NORMAL,
-	.index = 0x80 << 8 | POWERMATE_PULSE_SPEED_SLOW
+    .upper_value = POWERMATE_PULSE_TABLE_NORMAL,
+    .index = 0x80 << 8 | POWERMATE_PULSE_SPEED_SLOW
 };
 
 PowermateControl powermate_control_led_bright = {
-	.type = POWERMATE_CONTROL_STATIC_BRIGHTNESS,
-	.upper_value = 0,
-	.index = 0xff
+    .type = POWERMATE_CONTROL_STATIC_BRIGHTNESS,
+    .upper_value = 0,
+    .index = 0xff
 };
 
 PowermateControl powermate_control_led_dim = {
-	.type = POWERMATE_CONTROL_STATIC_BRIGHTNESS,
-	.upper_value = 0,
-	.index = 0x80
+    .type = POWERMATE_CONTROL_STATIC_BRIGHTNESS,
+    .upper_value = 0,
+    .index = 0x80
 };
 
 PowermateControl powermate_control_led_off = {
-	.type = POWERMATE_CONTROL_STATIC_BRIGHTNESS,
-	.upper_value = 0,
-	.index = POWERMATE_OFF
+    .type = POWERMATE_CONTROL_STATIC_BRIGHTNESS,
+    .upper_value = 0,
+    .index = POWERMATE_OFF
 };
 
 PowermateControl powermate_control_pulse_awake_on = {
-	.type = POWERMATE_CONTROL_PULSE_AWAKE,
-	.upper_value = 0,
-	.index = POWERMATE_ON
+    .type = POWERMATE_CONTROL_PULSE_AWAKE,
+    .upper_value = 0,
+    .index = POWERMATE_ON
 };
 
 PowermateControl powermate_control_pulse_awake_off = {
-	.type = POWERMATE_CONTROL_PULSE_AWAKE,
-	.upper_value = 0,
-	.index = POWERMATE_OFF
+    .type = POWERMATE_CONTROL_PULSE_AWAKE,
+    .upper_value = 0,
+    .index = POWERMATE_OFF
 };
 
 PowermateControl powermate_control_pulse_asleep_on = {
-	.type = POWERMATE_CONTROL_PULSE_ASLEEP,
-	.upper_value = 0,
-	.index = POWERMATE_ON
+    .type = POWERMATE_CONTROL_PULSE_ASLEEP,
+    .upper_value = 0,
+    .index = POWERMATE_ON
 };
 
 PowermateControl powermate_control_pulse_asleep_off = {
-	.type = POWERMATE_CONTROL_PULSE_ASLEEP,
-	.upper_value = 0,
-	.index = POWERMATE_OFF
+    .type = POWERMATE_CONTROL_PULSE_ASLEEP,
+    .upper_value = 0,
+    .index = POWERMATE_OFF
 };
--- test/test_powermate_hid.c
+++ test/test_powermate_hid.c
@@ -1,107 +1,109 @@
 #include "../vendor/ceedling/vendor/unity/src/unity.h"
 #include "powermate_hid.h"
+#include "libusb.h"
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 
 PowermateHid *test_hid;
 
 PowermateData test_data = {
-	.button_state = POWERMATE_OFF,
-	.knob_displacement = 0,
-	._unused = 0,
-	.led_brightness = 0x80,
-	.led_status = 0x00,
-	.led_multiplier = 1
+    .button_state = POWERMATE_OFF,
+    .knob_displacement = 0,
+    ._unused = 0,
+    .led_brightness = 0x00,
+    .led_status = 0x00,
+    .led_multiplier = 1
 };
 
 PowermateError test_error = POWERMATE_HID_SUCCESS;
 
 void setUp(void)
 {
-	test_hid = powermate_hid_new();
+    test_hid = powermate_hid_new();
+    TEST_ASSERT_NOT_NULL(test_hid);
 }
 
 void tearDown(void)
 {
-	if(test_hid != NULL)
-	{
-		powermate_hid_delete(test_hid);
-		test_hid = NULL;
-	}
-}
-
-void test_new(void)
-{
-	TEST_ASSERT_NOT_NULL(test_hid);
+    if(test_hid != NULL)
+    {
+        powermate_hid_delete(test_hid);
+        test_hid = NULL;
+    }
 }
 
 void send_control(PowermateControl control);
 
 void test_get_input(void)
 {
-	send_control(powermate_control_slow_pulse);
-	send_control(powermate_control_led_dim);
+    send_control(powermate_control_slow_pulse);
 
-	test_error = powermate_hid_get_input(test_hid);
+    send_control(powermate_control_led_dim);
+   
+    test_error = powermate_hid_get_input(test_hid);
 
-	TEST_ASSERT_EQUAL(POWERMATE_HID_SUCCESS, test_error);
-	TEST_ASSERT_EQUAL_MEMORY(
-		&test_data, &test_hid->last_input, 6);
+    TEST_ASSERT_EQUAL(POWERMATE_HID_SUCCESS, test_error);
+    TEST_ASSERT_EQUAL_MEMORY(
+        &test_data, &test_hid->last_input, 6);
 }
 
 void send_control(PowermateControl control)
 {
-	test_error = powermate_hid_set_control(test_hid, control);
+    test_error = powermate_hid_set_control(test_hid, control);
 
-	TEST_ASSERT_EQUAL(POWERMATE_HID_SUCCESS, test_error);
+    TEST_ASSERT_EQUAL(POWERMATE_HID_SUCCESS, test_error);
 
-	test_error = powermate_hid_send_output(test_hid);
+    test_error = powermate_hid_send_output(test_hid);
 
-	TEST_ASSERT_EQUAL(POWERMATE_HID_SUCCESS, test_error);
+    TEST_ASSERT_EQUAL(POWERMATE_HID_SUCCESS, test_error);
 
-	TEST_ASSERT_EQUAL_MEMORY(
-		&control, &test_hid->control, sizeof(PowermateControl));
+    TEST_ASSERT_EQUAL_MEMORY(
+        &control, &test_hid->control, sizeof(PowermateControl));
 }
 
 void test_send_pulse_awake_on(void)
 {
-	send_control(powermate_control_pulse_awake_on);
+    send_control(powermate_control_pulse_awake_on);
 }
 
 void test_send_fast_pulse(void)
 {
-	send_control(powermate_control_fast_pulse);
+    send_control(powermate_control_fast_pulse);
 }
 
 void test_send_slow_pulse(void)
 {
-	send_control(powermate_control_slow_pulse);
+    send_control(powermate_control_slow_pulse);
 }
 
 void test_send_led_off(void)
 {
-	send_control(powermate_control_led_off);
+    send_control(powermate_control_led_off);
 }
 
 void test_send_led_on(void)
 {
- 	send_control(powermate_control_led_dim);
+    send_control(powermate_control_led_dim);
 }
 
 void test_send_led_bright(void)
 {
-	send_control(powermate_control_led_bright);
+    send_control(powermate_control_led_bright);
 }
 
 void test_send_pulse_awake_off(void)
 {
-	send_control(powermate_control_pulse_awake_off);
+    send_control(powermate_control_pulse_awake_off);
 }
 
 void test_send_pulse_asleep_on(void)
 {
-	send_control(powermate_control_pulse_asleep_on);
+    send_control(powermate_control_pulse_asleep_on);
 }
 
 void test_send_pulse_asleep_off(void)
 {
-	send_control(powermate_control_pulse_asleep_off);
+    send_control(powermate_control_pulse_asleep_off);
 }
