0110531892d314bdd0d79ab8722d3c88ce0c476f
Austin Mullins <austin.mullins@gmail.com>
2014-07-01 22:49:42 -0500

    Fixed build script and .gitignore

--- build_powermate_client.sh
+++ build_powermate_client.sh
@@ -2,5 +2,5 @@
 
 rake test:all
 gcc -o build/test/out/main.o -c src/main.c -Isrc
-gcc -o powermate_client build/test/out/main.o \
+gcc -o bin/powermate_client build/test/out/main.o \
        build/test/out/powermate_hid.o -lhidapi-libusb -lusb-1.0
